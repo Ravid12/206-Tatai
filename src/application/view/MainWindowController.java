a6f381034eab9d13a80e2230ce1f9bb874fd217d
ravid12 <ravid12@gmail.com>
2017-09-10 12:44:37 +1200

    initial commit

---
+++ src/application/view/MainWindowController.java
@@ -0,0 +1,55 @@
+package application.view;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import application.controller.MainApp;
+
+public class MainWindowController {
+    @FXML
+    private Label firstNameLabel;
+    @FXML
+    private Label lastNameLabel;
+    @FXML
+    private Label streetLabel;
+    @FXML
+    private Label postalCodeLabel;
+    @FXML
+    private Label cityLabel;
+    @FXML
+    private Label birthdayLabel;
+
+    // Reference to the main application.
+    private MainApp mainApp;
+
+    /**
+     * The constructor.
+     * The constructor is called before the initialize() method.
+     */
+    public MainWindowController () {
+    }
+
+    /**
+     * Initializes the controller class. This method is automatically called
+     * after the fxml file has been loaded.
+     */
+    @FXML
+    private void initialize() {
+    }
+    
+    /**
+     * Called when the user clicks on the Easy button.
+     */
+    @FXML
+    private void handleEasySelection() {
+    	System.out.println("EASY!");
+    }
+
+    /**
+     * Is called by the main application to give a reference back to itself.
+     * 
+     * @param mainApp
+     */
+    public void setMainApp(MainApp mainApp) {
+        this.mainApp = mainApp;
+    }
+}
