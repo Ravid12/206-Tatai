6e7babf2b5e396f6b57fc5b883b3c3bbeb95a4aa
zinzan1 <zinzannzhao@gmail.com>
2017-09-26 09:13:40 +1300

    Added I/O to statsmodel, need to refactor

--- src/application/model/StatsModel.java
+++ src/application/model/StatsModel.java
@@ -1,5 +1,39 @@
 package application.model;
 
-public class StatsModel {
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class StatsModel 
+{
+	private ArrayList<String> al = new ArrayList<String>();
 	
+	public void readFile()
+	{
+		try {
+			File file = new File("test.txt");
+			FileReader fileReader = new FileReader(file);
+			BufferedReader bufferedReader = new BufferedReader(fileReader);
+			
+			String line;
+			while ((line = bufferedReader.readLine()) != null) {
+				al.add(line);
+				System.out.println(line.toString());
+				
+			}
+			fileReader.close();
+		} 
+		
+		catch (IOException e) 
+		{
+			e.printStackTrace();
+		}
+	}
+	
+	public ArrayList<String> StatsInfo()
+	{
+		return al;
+	}
 }
